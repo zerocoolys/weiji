864d36efeadf4d1e6a01b23a10a869ab988667b8
dolphineor <dolphineor@gmail.com>
2015-07-31 12:40:41 +0800

    Integrate socket.io with restify

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 var restify = require('restify');
+var socketIo = require('socket.io');
 var morgan = require('morgan');
 var server = restify.createServer();
-var db = require('mongojs')('weijiapp', ['Users', 'Posts', 'Locations'])
+var io = socketIo.listen(server.server);
+var db = require('mongojs')('weijiapp', ['Users', 'Posts', 'Locations']);
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
@@ -14,6 +16,19 @@ server.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+
+//================ Socket.io ================
+server.get('/', function indexHTML(req, res, next) {
+    next();
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', {hello: 'world'});
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
+//===========================================
 
 server.listen(process.env.PORT || 9804, function () {
 
